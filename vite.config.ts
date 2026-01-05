ab9d6b66caadd143aea6e5a3cb6422f92a618eab
lfgiacomelli <lfgiacomellirodrigues@gmail.com>
2026-01-05 09:56:42 -0300

    [fix] vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
